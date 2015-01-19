82c25b39ab273ff5a1e7bcb4e6c1d1bc54f66a7e
bluegrizzly <alain.dessureaux@gmail.com>
2015-01-18 19:45:46 -0500

    update batch file

--- importdb.bat
+++ importdb.bat
@@ -1,5 +1,14 @@
 @echo off 
-set root=C:\WineMapPub\
+set root=C:\WineManPub\
 cd %root%
-mysql -u root  -e "CREATE DATABASE IF NOT EXISTS wineman";
-mysql -u root  wineman < app_data\*.sql
+rem "C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root  -e "CREATE DATABASE IF NOT EXISTS wineman";
+
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_steps.sql
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_transaction_step.sql
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_transactions.sql
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_wine_brands.sql
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_wine_categories.sql
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_wine_types.sql
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_customers.sql
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_holidays.sql
+"C:\Program Files\MySQL\MySQL Server 5.6\bin\mysql" -ppiepasri -u root wineman < App_Data\wineman_settings.sql
